61581e57d3e15ac09809f97bb2cb8f76d2987d12
Md-Ferdous <55968913+Md-Ferdous@users.noreply.github.com>
2021-06-23 12:00:50 +0600

    Update train.py

--- train.py
+++ train.py
@@ -144,14 +144,15 @@ def main(args=None):
                 loss_hist.append(float(loss))
 
                 epoch_loss.append(float(loss))
-
+                print('Epoch: {} | Iteration: {} | Classification loss: {:1.5f} | Regression loss: {:1.5f} | Running loss: {:1.5f}'.format(
+                        epoch_num, iter_num, float(classification_loss), float(regression_loss), np.mean(loss_hist))end='\r') 
+                
                 del classification_loss
                 del regression_loss
             except Exception as e:
                 print(e)
                 continue
-        print('Epoch: {} | Iteration: {} | Classification loss: {:1.5f} | Regression loss: {:1.5f} | Running loss: {:1.5f}'.format(
-                        epoch_num, iter_num, float(classification_loss), float(regression_loss), np.mean(loss_hist)))        
+               
         if parser.dataset == 'coco':
 
             print('Evaluating dataset')
