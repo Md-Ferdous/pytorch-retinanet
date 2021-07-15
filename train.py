27ad9f5a0d343fb8564df9afdd2104a2cd11181e
Md-Ferdous <55968913+Md-Ferdous@users.noreply.github.com>
2021-07-15 11:23:11 +0600

    Update train.py

--- train.py
+++ train.py
@@ -86,7 +86,7 @@ def main(args=None):
         retinanet = model.resnet152(num_classes=dataset_train.num_classes(), pretrained=True)
     else:
         raise ValueError('Unsupported model depth, must be one of 18, 34, 50, 101, 152')
-    ratinanet.summary()
+    
     use_gpu = True
 
     if use_gpu:
